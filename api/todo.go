78cb68440550ad9a293041607d1cea92797b4d52
Muhammad Naufal Kateni <naufalkateni2001@gmail.com>
2025-06-14 15:14:05 +0700

    remove redelare

--- api/todo.go
+++ api/todo.go
@@ -4,23 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
-	"sync"
-)
-
-type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-}
-
-var (
-	Todos = map[int]Todo{
-		1: {ID: 1, Title: "Learn Go", Done: false},
-		2: {ID: 2, Title: "Build CRUD", Done: false},
-		3: {ID: 3, Title: "Connect from Next.js", Done: true},
-	}
-	NextID = 4
-	Mu     sync.Mutex
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
